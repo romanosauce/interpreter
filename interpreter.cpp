401cc74d59bedd01dba5230f5c2e2dccb3d428cd
Roman Danilov <romanosauce.code@gmail.com>
2023-04-12 00:47:28 +0300

    add declaration of reserved words table

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+
+enum Error {
+};
+
+std::unordered_set<std::string> reserved_words {
+    "program", "int", "real", "string", "boolean", "if", "else",
+    "while", "read", "write", "for", "break", "true", "false"
+};
+
+std::unordered_set<std::string> types {
+    "int", "real", "boolean"
+};
+
+std::unordered_set<char> delimeters {
+    ' ', '\n', '\t', '*', '%', '+', '-', '<', '>', '=', '!', ',',
+    ';', ':', '(', ')', '/'
+};
+
+class Identifier {
+    public:
+        std::string name, type;
+        std::variant<int, bool, std::string> value;
+};
 
 int main() {
     return 0;
