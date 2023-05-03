f3abb26846f727e5b685d98892b7bec18124dc10
Roman Danilov <romanosauce.code@gmail.com>
2023-05-03 21:53:15 +0300

    create structures and classes for lexical analysis
    In this commit another attempt to rework the lexical step was been made.
    At first glance, the most successful one

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,68 +1,23 @@
 #include <iostream>
-#include <sstream>
 #include <vector>
-#include <string>
-#include <unordered_map>
 #include <unordered_set>
-#include <variant>
-#include <fstream>
 
-enum class Error {
-    FILE_NO = -1,
-    FILE_BAD = -2
+using namespace std;
+
+enum class TypeOfLex {
+    LEX_NULL
 };
 
-std::vector<std::pair<Error, size_t>> error_stack;
-
-std::string data;
-size_t size;
-
-std::unordered_set<std::string> reserved_words {
-    "program", "int", "real", "string", "boolean", "if", "else",
-    "do", "while", "read", "write", "for", "break", "continue", "true",
-    "false", "not", "and", "or", "goto"
-};
-
-std::unordered_set<std::string> types {
-    "int", "real", "boolean"
-};
-
-std::unordered_set<char> delimeters {
-    ' ', '\n', '\t', '*', '%', '+', '-', '<', '>', '=', '!', ',',
-    ';', ':', '(', ')', '/'
-};
-
-class Identifier {
+class Lex {
     public:
-        std::string name, type;
-        std::variant<int, bool, std::string> value;
-};
+        Lex(TypeOfLex t = LEX_NULL, int v = 0) : t_lex_(t), v_lex_(v) {}
+        TypeOfLex get_type() const {
+            return t_lex_;
+        }
+        int get_value() const {
+            return v_lex_;
+        }
+    private:
+        TypeOfLex t_lex_;
+        int v_lex_;
 
-int ShowErrors();
-void skip_whitespace();
-
-int main(int argc, char **argv) {        
-    if (argc < 2) {
-        error_stack.push_back({ Error::FILE_NO, 0 });
-        return ShowErrors();
-    }
-
-    std::ifstream file(argv[1], std::ios::binary);
-    std::stringstream buf;
-    buf << file.rdbuf();
-    data = buf.str();
-    if (file.fail()) {
-        error_stack.push_back({ Error::FILE_BAD, 0 });
-        return ShowErrors();
-    }
-    std::cout << data << "\n";
-
-    return 0;
-}
-
-int ShowErrors() {
-    return 0;
-}
-
-void skip_whitespace() {
-}
